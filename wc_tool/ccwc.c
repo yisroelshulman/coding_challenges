4b4e5c05cc66cb3809178d00d1abe1d5b18f3d75
yisroel <yisroelshulman16@gmail.com>
2024-01-15 23:09:57 -0500

    added the wc_default function and helper functions. reworked so everything can be done froma single pass

--- wc_tool/ccwc.c
+++ wc_tool/ccwc.c
@@ -1,10 +1,53 @@
 #include <stdio.h>
+#include <stdbool.h>
 
 typedef enum {
     WC_SUCCESS,
     WC_FAIL
 } WCStatus;
 
+FILE* input_stream;
+
+static long byte_count = 0;
+static long line_count = 0;
+static long word_count = 0;
+static long char_count = 0;
+
+
+static char next() {
+    char c = fgetc(input_stream);
+    if (c == '\n') line_count ++;
+    byte_count++;
+    char_count++;
+    return c;
+}
+
+static bool is_whitespace(const char c) {
+    return c == ' ' || c == '\n'  || c == '\t' || c == '\r';
+}
+
+static char consume_white_space() {
+    char c = next();
+    while (is_whitespace(c)) c = next();
+    return c;
+}
+
+static void wc_default() {
+
+    char c = consume_white_space();
+    while (c != EOF) {
+        if (is_whitespace(c)) {
+            word_count++;
+            c = consume_white_space();
+        } else {
+            c = next();
+        }
+    }
+    // EOF is not considered.
+    char_count--;
+    byte_count--;
+    printf("bytes: %ld, lines: %ld, words: %ld, chars: %ld", byte_count, line_count, word_count, char_count);
+}
 
 WCStatus count_bytes(const char* file_name);
 WCStatus count_lines(const char* file_name);
@@ -14,14 +57,22 @@ WCStatus count_chars(const char* file_name);
 int main(const int argc, const char* argv[]) {
     printf("args: %d\n", argc);
 
-    WCStatus stat = WC_FAIL;
+   
 
     if (argc == 2) {
-        stat = count_chars(argv[1]);
-        if (stat == WC_SUCCESS) {
-            printf(" %s\n", argv[1]);
+        input_stream = fopen(argv[1], "r");
+        if (input_stream == NULL) {
+            // error
+            return 1;
         }
+    } else {
+        input_stream = stdin;
     }
+
+
+    wc_default();
+    printf(" [%s]\n", argv[1]);
+
 }
 
 WCStatus count_bytes(const char* file_name) {
