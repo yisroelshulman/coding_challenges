c7e0a426b12ce665dabf24b335ee439e64f021c6
yisroel <yisroelshulman16@gmail.com>
2024-01-15 20:53:38 -0500

    added the count chars, currently it does the same as the -c flag, need to look into multibyte chars

--- wc_tool/ccwc.c
+++ wc_tool/ccwc.c
@@ -9,6 +9,7 @@ typedef enum {
 WCStatus count_bytes(const char* file_name);
 WCStatus count_lines(const char* file_name);
 WCStatus count_words(const char* file_name);
+WCStatus count_chars(const char* file_name);
 
 int main(const int argc, const char* argv[]) {
     printf("args: %d\n", argc);
@@ -16,7 +17,7 @@ int main(const int argc, const char* argv[]) {
     WCStatus stat = WC_FAIL;
 
     if (argc == 2) {
-        stat = count_words(argv[1]);
+        stat = count_chars(argv[1]);
         if (stat == WC_SUCCESS) {
             printf(" %s\n", argv[1]);
         }
@@ -89,6 +90,25 @@ WCStatus count_words(const char* file_name) {
         }
     }
     count++; // last word
+    printf("%ld", count);
+    return WC_SUCCESS;
+}
+
+WCStatus count_chars(const char* file_name) {
+
+    FILE* fp =  fopen(file_name, "r");
+    if (fp == NULL) {
+        fprintf(stderr, "Can't open %s\n", file_name);
+        return WC_FAIL;
+    }
+
+    long count = 0;
+    char c;
+
+
+    while ((c = fgetc(fp)) != EOF) {
+        count++;
+    }
     printf("%ld", count);
     return WC_SUCCESS;
 }
