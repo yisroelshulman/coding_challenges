897ba9b5c043a58e0e7ec2e3b4873c09d926a851
yisroel <yisroelshulman16@gmail.com>
2024-01-15 20:14:47 -0500

    added the count_words and count_lines functions

--- wc_tool/ccwc.c
+++ wc_tool/ccwc.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 
-
 typedef enum {
     WC_SUCCESS,
     WC_FAIL
 } WCStatus;
 
+
 WCStatus count_bytes(const char* file_name);
+WCStatus count_lines(const char* file_name);
+WCStatus count_words(const char* file_name);
 
 int main(const int argc, const char* argv[]) {
     printf("args: %d\n", argc);
@@ -14,7 +16,7 @@ int main(const int argc, const char* argv[]) {
     WCStatus stat = WC_FAIL;
 
     if (argc == 2) {
-        stat = count_bytes(argv[1]);
+        stat = count_words(argv[1]);
         if (stat == WC_SUCCESS) {
             printf(" %s\n", argv[1]);
         }
@@ -41,5 +43,52 @@ WCStatus count_bytes(const char* file_name) {
     }
 
     printf("%ld", bytes);
+    return WC_SUCCESS;
+}
+
+WCStatus count_lines(const char* file_name) {
+
+    FILE* fp =  fopen(file_name, "r");
+    if (fp == NULL) {
+        fprintf(stderr, "Can't open %s\n", file_name);
+        return WC_FAIL;
+    }
+
+    char c;
+    long count = 0;
+    while ((c = fgetc(fp)) != EOF) {
+        if (c == '\n') count++;
+    }
+    
+    printf("%ld", count);
+    return WC_SUCCESS;
+}
+
+WCStatus count_words(const char* file_name) {
+
+    FILE* fp =  fopen(file_name, "r");
+    if (fp == NULL) {
+        fprintf(stderr, "Can't open %s\n", file_name);
+        return WC_FAIL;
+    }
+
+    char c = fgetc(fp);
+    long count = 0;
+    while(c == ' ' || c == '\n' || c == '\t' || c == '\r') {
+        c = fgetc(fp);
+    }
+
+    while ((c = fgetc(fp)) != EOF) {
+        if (c == ' ' || c == '\n' || c =='\t' || c == '\r') {
+            count++;
+            while(c == ' ' || c == '\n' || c == '\t' || c == '\r') {
+                c = fgetc(fp);
+                if (c == EOF) count--;
+            }
+
+        }
+    }
+    count++; // last word
+    printf("%ld", count);
     return WC_SUCCESS;
 }
