d5f73cda4df6c996f5bb7ea81827528080e13d8d
yisroel <yisroelshulman16@gmail.com>
2024-01-15 23:22:29 -0500

    removed extra code from ccwc.c and updated the makefile

--- wc_tool/ccwc.c
+++ wc_tool/ccwc.c
@@ -1,18 +1,12 @@
 #include <stdio.h>
 #include <stdbool.h>
 
-typedef enum {
-    WC_SUCCESS,
-    WC_FAIL
-} WCStatus;
-
 FILE* input_stream;
 
 static long byte_count = 0;
 static long line_count = 0;
 static long word_count = 0;
 static long char_count = 0;
-
 
 static char next() {
     char c = fgetc(input_stream);
@@ -26,7 +20,7 @@ static bool is_whitespace(const char c) {
     return c == ' ' || c == '\n'  || c == '\t' || c == '\r';
 }
 
-static char consume_white_space() {
+static char consume_whitespace() {
     char c = next();
     while (is_whitespace(c)) c = next();
     return c;
@@ -34,11 +28,11 @@ static char consume_white_space() {
 
 static void wc_default() {
 
-    char c = consume_white_space();
+    char c = consume_whitespace();
     while (c != EOF) {
         if (is_whitespace(c)) {
             word_count++;
-            c = consume_white_space();
+            c = consume_whitespace();
         } else {
             c = next();
         }
@@ -49,15 +43,8 @@ static void wc_default() {
     printf("bytes: %ld, lines: %ld, words: %ld, chars: %ld", byte_count, line_count, word_count, char_count);
 }
 
-WCStatus count_bytes(const char* file_name);
-WCStatus count_lines(const char* file_name);
-WCStatus count_words(const char* file_name);
-WCStatus count_chars(const char* file_name);
-
 int main(const int argc, const char* argv[]) {
     printf("args: %d\n", argc);
-
-   
 
     if (argc == 2) {
         input_stream = fopen(argv[1], "r");
@@ -69,97 +56,7 @@ int main(const int argc, const char* argv[]) {
         input_stream = stdin;
     }
 
-
     wc_default();
     printf(" [%s]\n", argv[1]);
 
-}
-
-WCStatus count_bytes(const char* file_name) {
-
-    FILE* fp =  fopen(file_name, "r");
-    if (fp == NULL) {
-        fprintf(stderr, "Can't open %s\n", file_name);
-        return WC_FAIL;
-    } 
-
-    if (fseek(fp, 0L, SEEK_END) != 0) {
-        fprintf(stderr, "error: fseek\n");
-        return WC_FAIL;
-    }
-
-    long bytes = ftell(fp);
-    if (bytes == -1) {
-        fprintf(stderr, "error: ftell\n");
-        return WC_FAIL;
-    }
-
-    printf("%ld", bytes);
-    return WC_SUCCESS;
-}
-
-WCStatus count_lines(const char* file_name) {
-
-    FILE* fp =  fopen(file_name, "r");
-    if (fp == NULL) {
-        fprintf(stderr, "Can't open %s\n", file_name);
-        return WC_FAIL;
-    }
-
-    char c;
-    long count = 0;
-    while ((c = fgetc(fp)) != EOF) {
-        if (c == '\n') count++;
-    }
-    
-    printf("%ld", count);
-    return WC_SUCCESS;
-}
-
-WCStatus count_words(const char* file_name) {
-
-    FILE* fp =  fopen(file_name, "r");
-    if (fp == NULL) {
-        fprintf(stderr, "Can't open %s\n", file_name);
-        return WC_FAIL;
-    }
-
-    char c = fgetc(fp);
-    long count = 0;
-    while(c == ' ' || c == '\n' || c == '\t' || c == '\r') {
-        c = fgetc(fp);
-    }
-
-    while ((c = fgetc(fp)) != EOF) {
-        if (c == ' ' || c == '\n' || c =='\t' || c == '\r') {
-            count++;
-            while(c == ' ' || c == '\n' || c == '\t' || c == '\r') {
-                c = fgetc(fp);
-                if (c == EOF) count--;
-            }
-
-        }
-    }
-    count++; // last word
-    printf("%ld", count);
-    return WC_SUCCESS;
-}
-
-WCStatus count_chars(const char* file_name) {
-
-    FILE* fp =  fopen(file_name, "r");
-    if (fp == NULL) {
-        fprintf(stderr, "Can't open %s\n", file_name);
-        return WC_FAIL;
-    }
-
-    long count = 0;
-    char c;
-
-
-    while ((c = fgetc(fp)) != EOF) {
-        count++;
-    }
-    printf("%ld", count);
-    return WC_SUCCESS;
 }
