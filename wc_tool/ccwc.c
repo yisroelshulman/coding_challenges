ff2691e870ea4f569c62fa93b423f6094efe0b09
yisroel <yisroelshulman16@gmail.com>
2024-01-16 21:04:27 -0500

    added the functionality for calling without any args (reading from stdin)

--- wc_tool/ccwc.c
+++ wc_tool/ccwc.c
@@ -8,6 +8,7 @@
 #define WC_LINE         1u << 1
 #define WC_WORD         1u << 2
 #define WC_CHAR         1u << 3
+#define WC_UNSET        1u << 4
 
 typedef struct {
     int byte_count;
@@ -16,6 +17,19 @@ typedef struct {
     int char_count;
     FILE* input_stream;
 } WCInfo;
+
+// assigning stream is safe because it is either stdin or was checked for NULL before the call to
+// this function
+static WCInfo init_wc_info(FILE* stream) {
+    WCInfo file = {
+        .byte_count = 0,
+        .line_count = 0,
+        .char_count = 0,
+        .word_count = 0,
+        .input_stream = stream
+    };
+    return file;
+}
 
 static char next(WCInfo* file) {
     char c = fgetc(file->input_stream);
@@ -29,18 +43,18 @@ static bool is_whitespace(const char c) {
     return c == ' ' || c == '\n'  || c == '\t' || c == '\r';
 }
 
-static char consume_whitespace(WCInfo* file) {
+static char consume_whitespaces(WCInfo* file) {
     char c = next(file);
     while (is_whitespace(c)) c = next(file);
     return c;
 }
 
 static void wc_default(WCInfo* file) {
-    char c = consume_whitespace(file);
+    char c = consume_whitespaces(file);
     while (c != EOF) {
         if (is_whitespace(c)) {
             file->word_count++;
-            c = consume_whitespace(file);
+            c = consume_whitespaces(file);
         } else {
             c = next(file);
         }
@@ -48,10 +62,10 @@ static void wc_default(WCInfo* file) {
     // EOF was added in next(), and is not considered.
     file->char_count--;
     file->byte_count--;
-    printf("bytes: %d, lines: %d, words: %d, chars: %d\n", file->byte_count, file->line_count, file->word_count, file->char_count);
 }
 
-static uint8_t set_flags(const uint8_t flag, const char* flags) {
+static uint8_t set_flags(uint8_t flag, const char* flags) {
+    if (flag&WC_UNSET) flag = 0;
     uint8_t current_flag = 0;
     int pos = 1;
     char c = flags[pos++]; // first char
@@ -69,52 +83,52 @@ static uint8_t set_flags(const uint8_t flag, const char* flags) {
     return flag|current_flag;
 }
 
-static void print_flags(const uint8_t flag) {
-    if (flag&WC_BYTES) {
-        printf("byte set ");
+static void print_result(const WCInfo* file, const uint8_t flags) {
+    if (flags&WC_LINE) {
+        printf("\t%d", file->line_count);
     }
-    if (flag&WC_LINE) {
-        printf("line set ");
+    if (flags&WC_WORD) {
+        printf("\t%d", file->word_count);
     }
-    if (flag&WC_WORD) {
-        printf("word set ");
+    if (flags&WC_BYTES) {
+        printf("\t%d", file->byte_count);
     }
-    if (flag&WC_CHAR) {
-        printf("char set ");
+    if (flags&WC_CHAR) {
+        printf("\t%d", file->char_count);
     }
     printf("\n");
 }
 
 int main(const int argc, const char* argv[]) {
 
-    uint8_t flag = 0;
+    uint8_t flags = WC_UNSET;
+    uint8_t num_files = 0; // arbitraty max num files to 256
 
     for (int i = 1; i < argc; i++) {
         if (argv[i][0] == '-') {
-            flag = set_flags(flag, argv[i]);
-            if (flag == WC_INVALID_FLAG) {
+            flags = set_flags(flags, argv[i]);
+            if (flags == WC_INVALID_FLAG) {
                 printf("invalid flag [%s]\n", argv[i]);
                 return 1;
             }
         } else {
-            WCInfo file = {
-                .byte_count = 0,
-                .line_count = 0,
-                .word_count = 0,
-                .char_count = 0,
-                .input_stream = fopen(argv[i], "r")
-            };
-            if (file.input_stream == NULL) {
-                printf("[%s] No such file or directory\n", argv[i]);
+            num_files++;
+            FILE* stream = fopen(argv[i], "r");
+            if (stream == NULL) {
+                printf("[%s] No such file or directory.\n", argv[i]);
             } else {
+                WCInfo file = init_wc_info(stream);
                 wc_default(&file);
+                file.input_stream = NULL;
+                fclose(stream);
             }
         }
     }
-    print_flags(flag);
 
-    
-    //wc_default();
-    printf(" [%s]\n", argv[1]);
-
+    if (num_files == 0) {
+        if (flags&WC_UNSET) flags = 0b111; // setting the proper bytes to 1
+        WCInfo file = init_wc_info(stdin);
+        wc_default(&file);
+        print_result(&file, flags);
+    }
 }
